d322d40c0c5a5e967cf281a99d06210d5d96c854
Antonio Terceiro <terceiro@softwarelivre.org>
2013-01-29 22:15:08 -0300

    cleanup terminal

--- lib/kodo/web/backend.rb
+++ lib/kodo/web/backend.rb
@@ -17,6 +17,9 @@ module Kodo
       end
 
       def self.start!
+        # FIXME put these log files in proper locations
+        stderr.reopen(File.new('/tmp/kodo.stderr', 'w'))
+        $stdout.reopen(File.new('/tmp/kodo.stdout', 'w'))
         exec('rackup', '-p', '20000', File.join(File.dirname(__FILE__), 'backend.ru'))
       end
 
--- lib/kodo/web/frontend.rb
+++ lib/kodo/web/frontend.rb
@@ -5,6 +5,8 @@ module Kodo
     module Frontend
 
       def self.run!
+        # FIXME here we should just open a GTK3 application with a webkit
+        # container inside. This does the trick for now
         datadir = File.join(Dir.home, '.cache', 'kodo', 'chromium')
         system('chromium', '--app=' + 'http://localhost:20000/', '--user-data-dir=' + datadir)
       end
