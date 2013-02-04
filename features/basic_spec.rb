8d4bd6577cef5861f8ffbc45b5d10a5b277415b0
Antonio Terceiro <terceiro@softwarelivre.org>
2013-02-03 22:16:07 -0300

    Basic nevigation between the screens
    - Open project
    - Visualization
    - Settings

--- features/basic_spec.rb
+++ features/basic_spec.rb
@@ -1,9 +1,26 @@
 require 'feature_helper'
 
-describe 'basics', :type => 'feature', :js => true do
-  it 'should display a title' do
+describe 'SourceBoard', :type => 'feature', :js => true do
+
+  it 'navigates between screens' do
     visit '/'
-    find('title').should_not be_nil
-    page.body.should match('<title>source_board</title>')
+    find('#openproject').should be_visible
+
+    find('.open-project').click
+    find('#openproject').should_not be_visible
+    find('#visualization').should be_visible
+
+    find('.settings').click
+    find("#settings").should be_visible
+    find('#visualization').should_not be_visible
+
+    find('.save-settings').click
+    find('#settings').should_not be_visible
+    find('#visualization').should be_visible
+
+    find('.change-project').click
+    find('#visualization').should_not be_visible
+    find('#openproject').should be_visible
   end
+
 end
--- lib/source_board/web/assets/js/sourceboard.js.coffee
+++ lib/source_board/web/assets/js/sourceboard.js.coffee
@@ -1,2 +1,27 @@
 #= require jquery
 #= require bootstrap
+
+$ ->
+  $('#openproject').show()
+  $('#visualization').hide()
+  $('#settings').hide()
+
+$('#openproject .btn').click ->
+  $('#openproject').hide()
+  $('#visualization').show()
+  return false
+
+$('#visualization .settings').click ->
+  $('#visualization').hide()
+  $('#settings').show()
+  return false
+
+$('#visualization .change-project').click ->
+  $('#visualization').hide()
+  $('#openproject').show()
+  return false
+
+$('#settings .btn').click ->
+  $('#settings').hide()
+  $('#visualization').show()
+  return false
