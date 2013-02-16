adad389c1ea0a71a2ec934e4f9c79d0f2d644483
Antonio Terceiro <terceiro@softwarelivre.org>
2013-02-16 13:53:00 -0300

    Move default value behavior to extraction methods

--- lib/source_board/parser/simple.rb
+++ lib/source_board/parser/simple.rb
@@ -20,7 +20,7 @@ module SourceBoard
             'name'      => File.basename(file.path),
             'metrics'   => {
               'loc'     => loc_data[file.path],
-              'nom'     => nom_data[file.path] || 0,
+              'nom'     => nom_data[file.path],
             },
           }
         end
@@ -36,7 +36,7 @@ module SourceBoard
       end
 
       def extract_nom_data(source_tree)
-        process_pipe(source_tree, 'xargs ctags -x') do |data,line|
+        process_pipe(source_tree, 'xargs ctags -x', 0) do |data,line|
           tag, kind, line_number, path, signature = line.split
           data[path] ||= 0
           if ["member", "function", "method"].include?(kind)
@@ -45,13 +45,13 @@ module SourceBoard
         end
       end
 
-      def process_pipe(source_tree, command)
+      def process_pipe(source_tree, command, default_value = nil)
         pipe = IO.popen(command, 'w+')
         source_tree.files.each do |source_file|
           pipe.puts source_file.path
         end
         pipe.close_write
-        data = {}
+        data = Hash.new(default_value)
         pipe.each_line do |line|
           yield(data, line)
         end
